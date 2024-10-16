ab55ee349cb004bffcadb0ecbf5c1d3a1ef63b69
DIego CM <diegocangas6@gmail.com>
2024-10-16 19:57:32 +0200

    Add files via upload

---
+++ AutoIncrementBuildVersion.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Diagnostics;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+public class AutoIncrementBuildVersion : IPreprocessBuildWithReport
+{
+	public int callbackOrder { get { return 0; } }
+	public void OnPreprocessBuild (BuildReport buildTarget)
+	{
+		string currentVersion = PlayerSettings.bundleVersion;
+		try
+		{
+			int tipoDeVersion = EditorUtility.DisplayDialogComplex("Version",
+				"¿Que tipo de actualización intenta compilar?", "Mayor (X.0.0)", "Parche (0.0.X)", "Menor (0.X.0)");
+			int major = Convert.ToInt32 (currentVersion.Split ('.') [0]);
+			int minor = Convert.ToInt32 (currentVersion.Split ('.') [1]);
+			int build = Convert.ToInt32 (currentVersion.Split ('.') [2]);
+
+			if (tipoDeVersion == 0) major++;
+			else if (tipoDeVersion == 1) build++;
+			else if (tipoDeVersion == 2) minor++;
+			
+			PlayerSettings.bundleVersion = major + "." + minor + "." + build;
+
+			if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.iOS) {
+				PlayerSettings.iOS.buildNumber = ""+int.Parse(PlayerSettings.iOS.buildNumber)+1;
+				UnityEngine.Debug.Log ("BuidNumber:" + PlayerSettings.iOS.buildNumber + " y version" + PlayerSettings.bundleVersion);
+
+			} else if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.Android) {
+				++PlayerSettings.Android.bundleVersionCode;
+				UnityEngine.Debug.Log ("BundelVersion:" + PlayerSettings.Android.bundleVersionCode + " y version" + PlayerSettings.bundleVersion);
+			}
+			else
+			{
+				UnityEngine.Debug.Log ("Version: " + PlayerSettings.bundleVersion);
+			}
+		} catch (Exception e) {
+			UnityEngine.Debug.LogError (e);
+			UnityEngine.Debug.LogError ("Fallo al ejecutar el script");
+		}
+	}
+}
+
+
+
